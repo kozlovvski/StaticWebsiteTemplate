23bc3c80b870c7d47c3a5b900c14f56d29cb3f89
kozlovvski <mcbiggos@gmail.com>
2019-06-09 13:30:00 +0200

    added deploy to gh-pages from /dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV;
 
 module.exports = env => {
-  console.log(`🛠️  Ahoy there! Building with ${env} mode using ./webpack/webpack.${env}.js 🛠️`);
+  console.log(`🛠️  Ahoy there! Building with ${env} mode using ./webpack/webpack.${env}.js 🛠️  `);
   return require(`./webpack/webpack.${env}.js`);
 };
